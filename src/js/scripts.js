2f9d79928791bfc8c2eb65acf9f335e7da065af5
InsanityMeetsHH <software@insanitymeetshh.net>
2018-05-03 17:10:16 +0200

    Added lazyLoad example and cssua monitor.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,13 +3,19 @@
     $(document).ready(function(){
         $('html').removeClass('no-js');
         $('[data-toggle="tooltip"]').tooltip();
+        
         $('.slider-brands').slick({
             dots: true,
             autoplay: true,
             autoplaySpeed: 2000
         });
+        
         new LazyLoad({
-            elements_selector: ".lazyload"
+//            elements_selector: ".lazyload"
+        });
+        
+        $.each(cssua.ua, function(key, value) {
+            $('.cssua-monitor').append(key + ': ' + value + '<br>');
         });
     });
 })(jQuery);
