0bcdad75eb5e99dd8077ee6dc966f2f5bbc44755
InsanityMeetsHH <software@insanitymeetshh.net>
2018-05-15 15:38:47 +0200

    First defined template is default template.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,6 @@ $templates = array(
     'index',
 );
 
-$template = isset($_GET['t']) && in_array($_GET['t'], $templates) ? $_GET['t'] : 'index';
+$template = isset($_GET['t']) && in_array($_GET['t'], $templates) ? $_GET['t'] : $templates[0];
 
 include __DIR__ . '/../templates/layouts/base.phtml';
