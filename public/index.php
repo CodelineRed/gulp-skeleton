a2540ad17991a43b941b7ed05edb8d2cc3c6a575
InsanityMeetsHH <software@insanitymeetshh.net>
2018-05-23 19:00:27 +0200

    Added template layout pairing in index.php.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 // available templates
 $templates = array(
-    'index',
+    'index' => 'base',
 );
+$templateKeys = array_keys($templates);
 
-$template = isset($_GET['t']) && in_array($_GET['t'], $templates) ? $_GET['t'] : $templates[0];
+$template = isset($_GET['t']) && in_array($_GET['t'], $templateKeys) ? $_GET['t'] : $templateKeys[0];
 
-include __DIR__ . '/../templates/layouts/base.phtml';
+include __DIR__ . '/../templates/layouts/' . $templates[$template] . '.phtml';
